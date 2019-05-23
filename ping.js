f0b7b25a4144d65b1db2ff8ddbda538a112ea123
Sami Hamed <sami.k.hamed@googlemail.com>
2019-05-23 12:57:53 +0200

    :tada: Setup ping-machine

---
+++ ping.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+module.exports = {
+    startInterval: function() {
+        const interval = 60000
+        console.log(`Setting interval to ${interval / 1000} seconds...`)
+        setInterval(ping, interval)
+        console.log('Sending initial ping...')
+        ping()
+
+        function ping() {
+            console.log('ping')
+            http.get('http://wayl-crawler.herokuapp.com')
+        }
+    }
+}
